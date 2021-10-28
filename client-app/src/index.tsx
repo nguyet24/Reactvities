f49c2e3137ecd59a2e6748219979c2020604d852
nguyet24 <52306481+nguyet24@users.noreply.github.com>
2021-10-28 11:25:46 +1100

    Duck Changes

---
+++ client-app/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+//React.strictmode will enforce anything that is our of date from react 17
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')//This tells react to get the info from the DIV 
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
